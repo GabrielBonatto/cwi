8902834fd4519c69e55624cd73efb529725eec9d
GabrielBonatto <88838468+GabrielBonatto@users.noreply.github.com>
2021-08-22 19:22:49 -0300

    Create AULA 5.av.js

---
+++ AULA 5.av.js
@@ -0,0 +1,13 @@
+saldoDaConta =  1000.00 
+valorcalca =  99.90
+valorsalario =  2500.00 
+esmolamendigo =  0.10 
+saldoDaConta = saldoDaConta - valorcalca + valorsalario - esmolamendigo 
+
+
+arcondicionado = (saldoDaConta *0.25)
+
+saldoDaConta = saldoDaConta - arcondicionado
+
+pagamentodeDivida = saldoDaConta / 2
+
